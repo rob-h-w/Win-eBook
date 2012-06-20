a638e38ec7a4777491ef23c351d3cfc4ced42f15
Rob Williamson <rob@robwilliamson.com>
2012-06-20 07:17:55 +0200

    Adding interfaces.
    To standardize eBook parsing.

---
+++ EBookData/IBook.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EBookData
+{
+    public interface IBook
+    {
+        string Author { get; }
+        IChapter [] Chapters { get; }
+        ICover Cover { get; }
+        string ISBN { get; }
+        string Language { get; }
+        string Path { get; }
+        DateTime PublicationDate { get; }
+        string Publisher { get; }
+        string Legal { get; }
+        string Title { get; }
+    }
+}
